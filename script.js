4c68770cb10eb30b1e8eedbe514aaff3bd3a72b1
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-06-16 18:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
+	let b =[];
+
+    while(a>0)
+    {
+        let rem = a%2;
+        b.unshift(rem);
+        a = Math.floor(a/2);
+    }
+    console.log(b.join(""));
   
 }
 
